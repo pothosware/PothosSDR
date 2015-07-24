0c42e7f81be19e44bf178bbd6c256a794b748c1f
Josh Blum <josh@joshknows.com>
2015-07-23 22:34:20 -0700

    build volk as it own module + patches

--- BuildGNURadio.cmake
+++ BuildGNURadio.cmake
@@ -3,31 +3,48 @@
 ##
 ## This script builds GNU Radio and toolkit bindings
 ##
+## * volk
 ## * gnuradio
 ## * gr-pothos (toolkit bindings project)
 ############################################################
 
+set(VOLK_BRANCH v1.0.1)
 set(GNURADIO_BRANCH v3.7.7.1_pothos1)
 set(GR_POTHOS_BRANCH master)
 
 #Use Python27 for Cheetah templates support
 set(PYTHON2_EXECUTABLE C:/Python27/python.exe)
 
-#commands for patching volk for msvc 2012 support
-#patching the submodule adds additional complexity
-if (MSVC11)
-set(VOLK_PATCH_COMMAND
-    ${GIT_EXECUTABLE} checkout ${GNURADIO_BRANCH} &&
-    ${GIT_EXECUTABLE} pull origin ${GNURADIO_BRANCH} &&
-    ${GIT_EXECUTABLE} submodule update && cd volk &&
-    ${GIT_EXECUTABLE} apply ${PROJECT_SOURCE_DIR}/patches/volk_skip_profile_app_vc11.diff &&
-    ${GIT_EXECUTABLE} commit -am "commit patches" && cd .. &&
-    ${GIT_EXECUTABLE} checkout --detach &&
-    ${GIT_EXECUTABLE} commit -am "commit submodules"
+############################################################
+## Build Volk
+############################################################
+message(STATUS "Configuring volk - ${VOLK_BRANCH}")
+ExternalProject_Add(volk
+    DEPENDS Pothos
+    GIT_REPOSITORY https://github.com/gnuradio/volk.git
+    GIT_TAG ${VOLK_BRANCH}
+    PATCH_COMMAND
+        ${GIT_EXECUTABLE} checkout . &&
+        ${GIT_EXECUTABLE} apply ${PROJECT_SOURCE_DIR}/patches/volk_config_log2_vc11.diff &&
+        ${GIT_EXECUTABLE} apply ${PROJECT_SOURCE_DIR}/patches/volk_skip_profile_app_vc11.diff
+    CMAKE_GENERATOR ${CMAKE_GENERATOR}
+    CMAKE_ARGS
+        -Wno-dev
+        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+        -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
+        -DBOOST_ROOT=${BOOST_ROOT}
+        -DBOOST_LIBRARYDIR=${BOOST_LIBRARYDIR}
+        -DBOOST_ALL_DYN_LINK=TRUE
+        -DPYTHON_EXECUTABLE=${PYTHON2_EXECUTABLE}
+    BUILD_COMMAND ${CMAKE_COMMAND} --build . --config ${CMAKE_BUILD_TYPE}
+    INSTALL_COMMAND ${CMAKE_COMMAND} --build . --config ${CMAKE_BUILD_TYPE} --target install
 )
-else (MSVC11)
-set(VOLK_PATCH_COMMAND cd .) #other versions -> do nothing
-endif (MSVC11)
+
+ExternalProject_Get_Property(volk SOURCE_DIR)
+install(
+    FILES ${SOURCE_DIR}/COPYING
+    DESTINATION licenses/volk
+)
 
 ############################################################
 ## Build GNU Radio
@@ -37,15 +54,15 @@ endif (MSVC11)
 ############################################################
 message(STATUS "Configuring GNURadio - ${GNURADIO_BRANCH}")
 ExternalProject_Add(GNURadio
-    DEPENDS Pothos
+    DEPENDS Pothos volk
     GIT_REPOSITORY https://github.com/pothosware/gnuradio.git
     GIT_TAG ${GNURADIO_BRANCH}
-    PATCH_COMMAND ${VOLK_PATCH_COMMAND}
     CMAKE_GENERATOR ${CMAKE_GENERATOR}
     CMAKE_ARGS
         -Wno-dev
         -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
         -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
+        -DENABLE_INTERNAL_VOLK=OFF
         -DBOOST_ROOT=${BOOST_ROOT}
         -DBOOST_LIBRARYDIR=${BOOST_LIBRARYDIR}
         -DBOOST_ALL_DYN_LINK=TRUE
