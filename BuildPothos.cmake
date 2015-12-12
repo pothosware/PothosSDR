cdd053765b961c344f1560f2fbd8f30fb31a212a
Josh Blum <josh@joshknows.com>
2015-12-11 19:37:54 -0800

    update to spuce 0.4.2

--- BuildPothos.cmake
+++ BuildPothos.cmake
@@ -8,7 +8,7 @@
 ############################################################
 
 set(POCO_BRANCH poco-1.6.1-release)
-set(SPUCE_BRANCH 0.4.1)
+set(SPUCE_BRANCH 0.4.2)
 set(POTHOS_SERIALIZATION_BRANCH pothos-serialization-0.2.0)
 set(POTHOS_BRANCH master)
 
