5c879e062509cb21b16cc13c1244a848ce801187
Josh Blum <josh@joshknows.com>
2015-12-12 21:48:13 -0800

    build muparserx for pothos

--- BuildPothos.cmake
+++ BuildPothos.cmake
@@ -4,11 +4,14 @@
 ## This script builds Poco (dependency) and Pothos project
 ##
 ## * poco (dependency)
+## * spuce (dependency)
+## * muparserx (dependency)
 ## * pothos (top level project)
 ############################################################
 
 set(POCO_BRANCH poco-1.6.1-release)
 set(SPUCE_BRANCH 0.4.2)
+set(MUPARSERX_BRANCH master)
 set(POTHOS_SERIALIZATION_BRANCH pothos-serialization-0.2.0)
 set(POTHOS_BRANCH master)
 
@@ -62,6 +65,29 @@ install(
 )
 
 ############################################################
+## Build muparserx
+############################################################
+message(STATUS "Configuring muparserx - ${MUPARSERX_BRANCH}")
+ExternalProject_Add(muparserx
+    GIT_REPOSITORY https://github.com/pothosware/muparserx.git
+    GIT_TAG ${MUPARSERX_BRANCH}
+    CMAKE_GENERATOR ${CMAKE_GENERATOR}
+    CMAKE_ARGS
+        -Wno-dev
+        -DBUILD_SHARED_LIBS=OFF
+        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+        -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
+    BUILD_COMMAND ${CMAKE_COMMAND} --build . --config ${CMAKE_BUILD_TYPE}
+    INSTALL_COMMAND ${CMAKE_COMMAND} --build . --config ${CMAKE_BUILD_TYPE} --target install
+)
+
+ExternalProject_Get_Property(muparserx SOURCE_DIR)
+install(
+    FILES ${SOURCE_DIR}/License.txt
+    DESTINATION licenses/muparserx
+)
+
+############################################################
 ## Build Pothos Serialization
 ############################################################
 message(STATUS "Configuring PothosSerialization - ${POTHOS_SERIALIZATION_BRANCH}")
@@ -90,7 +116,7 @@ install(
 ############################################################
 message(STATUS "Configuring Pothos - ${POTHOS_BRANCH}")
 ExternalProject_Add(Pothos
-    DEPENDS Poco SoapySDR Spuce PothosSerialization
+    DEPENDS Poco SoapySDR Spuce PothosSerialization muparserx
     GIT_REPOSITORY https://github.com/pothosware/pothos.git
     GIT_TAG ${POTHOS_BRANCH}
     CMAKE_GENERATOR ${CMAKE_GENERATOR}
