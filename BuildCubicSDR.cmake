f2cb4cd1b355a4d7ce8de19bcf3da82f39eebf4c
Josh Blum <josh@joshknows.com>
2016-02-09 15:16:32 -0800

    cubic sdr install exe + soapy dir

--- BuildCubicSDR.cmake
+++ BuildCubicSDR.cmake
@@ -8,6 +8,10 @@
 
 set(CUBIC_SDR_BRANCH master)
 
+if ("${MSVC_VERSION}" VERSION_LESS "1800")
+    return()
+endif()
+
 ############################################################
 ## Build CubicSDR
 ##
@@ -28,9 +32,10 @@ ExternalProject_Add(CubicSDR
         -DwxWidgets_LIB_DIR=${wxWidgets_LIB_DIR}
         -DFFTW_INCLUDES=${FFTW3F_INCLUDE_DIRS}
         -DFFTW_LIBRARIES=${FFTW3F_LIBRARIES}
+        -DSoapySDR_DIR=${CMAKE_INSTALL_PREFIX}
     BUILD_COMMAND ${CMAKE_COMMAND} --build . --config ${CMAKE_BUILD_TYPE}
     #INSTALL_COMMAND ${CMAKE_COMMAND} --build . --config ${CMAKE_BUILD_TYPE} --target install
-    INSTALL_COMMAND echo "TODO"
+    INSTALL_COMMAND echo "FIXME: external install rules are used in place of install target"
 )
 
 ExternalProject_Get_Property(CubicSDR SOURCE_DIR)
@@ -39,8 +44,11 @@ install(
     DESTINATION licenses/CubicSDR
 )
 
+ExternalProject_Get_Property(CubicSDR BINARY_DIR)
 install(
-    FILES ${SOURCE_DIR}/external/liquid-dsp/msvc/64/libliquid.dll
+    FILES
+        ${SOURCE_DIR}/external/liquid-dsp/msvc/64/libliquid.dll
+        ${BINARY_DIR}/x64/${CMAKE_BUILD_TYPE}/CubicSDR.exe
     DESTINATION bin
 )
 
