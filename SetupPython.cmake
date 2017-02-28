92babc653924641a2aefef45f047fd441d2f5736
Josh Blum <josh@joshknows.com>
2017-02-28 11:09:00 -0800

    find python from registry

--- SetupPython.cmake
+++ SetupPython.cmake
@@ -7,15 +7,25 @@
 ## * python3.6
 ############################################################
 
-set(PYTHON2_EXECUTABLE C:/Python27/python.exe)
-set(PYTHON2_INCLUDE_DIR C:/Python27/include)
-set(PYTHON2_LIBRARY C:/Python27/libs/python27.lib)
+get_filename_component(PYTHON2_ROOT "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.7\\InstallPath]" ABSOLUTE)
+if (NOT EXISTS "${PYTHON2_ROOT}")
+    message(FATAL_ERROR "Cant find python 2.7 InstallPath")
+endif()
+set(PYTHON2_EXECUTABLE ${PYTHON2_ROOT}/python.exe)
+set(PYTHON2_INCLUDE_DIR ${PYTHON2_ROOT}/include)
+set(PYTHON2_LIBRARY ${PYTHON2_ROOT}/libs/python27.lib)
 set(PYTHON2_INSTALL_DIR lib/python2.7/site-packages)
+message(STATUS "PYTHON2_ROOT: ${PYTHON2_ROOT}")
 
-set(PYTHON3_EXECUTABLE C:/Python36/python.exe)
-set(PYTHON3_INCLUDE_DIR C:/Python36/include)
-set(PYTHON3_LIBRARY C:/Python36/libs/python36.lib)
+get_filename_component(PYTHON3_ROOT "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\3.6\\InstallPath]" ABSOLUTE)
+if (NOT EXISTS "${PYTHON3_ROOT}")
+    message(FATAL_ERROR "Cant find python 3.6 InstallPath")
+endif()
+set(PYTHON3_EXECUTABLE ${PYTHON3_ROOT}/python.exe)
+set(PYTHON3_INCLUDE_DIR ${PYTHON3_ROOT}/include)
+set(PYTHON3_LIBRARY ${PYTHON3_ROOT}/libs/python36.lib)
 set(PYTHON3_INSTALL_DIR lib/python3.6/site-packages)
+message(STATUS "PYTHON3_ROOT: ${PYTHON3_ROOT}")
 
 set(CPACK_NSIS_EXTRA_INSTALL_COMMANDS "${CPACK_NSIS_EXTRA_INSTALL_COMMANDS}
 SetRegView 64
