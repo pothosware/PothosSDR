19c8ef66b7d2778008dbc67a5514eb0e05ac3b28
Josh Blum <josh@joshknows.com>
2017-07-27 03:44:14 -0500

    gnuradio path for grc tmpl attr

--- BuildGNURadio.cmake
+++ BuildGNURadio.cmake
@@ -57,6 +57,8 @@ MyExternalProject_Add(GNURadio
     DEPENDS volk uhd CppZMQ PortAudio CppUnit gsl fftw swig
     GIT_REPOSITORY https://github.com/gnuradio/gnuradio.git
     GIT_TAG ${GNURADIO_BRANCH}
+    PATCH_COMMAND ${GIT_PATCH_HELPER} --git ${GIT_EXECUTABLE}
+        ${PROJECT_SOURCE_DIR}/patches/grc_template_attr_issue.diff
     CMAKE_DEFAULTS ON
     CMAKE_ARGS
         -Wno-dev
