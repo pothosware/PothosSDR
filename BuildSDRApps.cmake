cdbd13f9fdd4d7af46aea4566439d86c6a66e2a5
Josh Blum <josh@joshknows.com>
2017-11-26 12:14:22 -0600

    move limesuite build to sdr apps

--- BuildSDRApps.cmake
+++ BuildSDRApps.cmake
@@ -1,14 +1,17 @@
 ############################################################
 ## Pothos SDR environment build sub-script
 ##
-## This script builds SDR applications
+## This script builds SDR applications that take a variety
+## of dependencies and didn't fit into the other Build* files.
 ##
 ## * CubicSDR
 ## * Inspectrum
+## * LimeSuite
 ############################################################
 
 set(CUBIC_SDR_BRANCH master)
 set(INSPECTRUM_SDR_BRANCH master)
+set(LIME_SUITE_BRANCH master)
 
 ############################################################
 ## Build CubicSDR
@@ -62,3 +65,33 @@ MyExternalProject_Add(Inspectrum
 
 list(APPEND CPACK_PACKAGE_EXECUTABLES "inspectrum" "Inspectrum")
 list(APPEND CPACK_CREATE_DESKTOP_LINKS "inspectrum")
+
+############################################################
+## Build LimeSuite
+##
+## -DWX_ROOT_DIR is a hack to prevent FindwxWidgets.cmake
+## from clearing wxWidgets_LIB_DIR the first configuration.
+##
+## -DFX3_SDK_PATH specifies the USB support for LimeSuite
+## If the SDK is not present, USB support will be disabled.
+##
+############################################################
+MyExternalProject_Add(LimeSuite
+    DEPENDS SoapySDR wxWidgets
+    GIT_REPOSITORY https://github.com/myriadrf/LimeSuite.git
+    GIT_TAG ${LIME_SUITE_BRANCH}
+    CMAKE_DEFAULTS ON
+    CMAKE_ARGS
+        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+        -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
+        -DLIME_SUITE_EXTVER=${EXTRA_VERSION_INFO}
+        -DWX_ROOT_DIR=${wxWidgets_ROOT_DIR}
+        -DwxWidgets_ROOT_DIR=${wxWidgets_ROOT_DIR}
+        -DwxWidgets_LIB_DIR=${wxWidgets_LIB_DIR}
+        -DSoapySDR_DIR=${CMAKE_INSTALL_PREFIX}
+        -DFX3_SDK_PATH=${FX3_SDK_PATH}
+    LICENSE_FILES COPYING
+)
+
+list(APPEND CPACK_PACKAGE_EXECUTABLES "LimeSuiteGUI" "Lime Suite")
+list(APPEND CPACK_CREATE_DESKTOP_LINKS "LimeSuiteGUI")
