ef37e6c876769ad6127912d3804523fd8f73cfab
Josh Blum <josh@joshknows.com>
2018-09-12 20:32:44 -0500

    bladef upstreamed this patch

--- BuildHwDrivers.cmake
+++ BuildHwDrivers.cmake
@@ -87,8 +87,6 @@ MyExternalProject_Add(bladeRF
     DEPENDS Pthreads libusb Pthreads
     GIT_REPOSITORY https://github.com/Nuand/bladeRF.git
     GIT_TAG ${BLADERF_BRANCH}
-    PATCH_COMMAND ${GIT_PATCH_HELPER} --git ${GIT_EXECUTABLE}
-        ${PROJECT_SOURCE_DIR}/patches/bladerf_fix_empty_array.diff
     CONFIGURE_COMMAND
         "${CMAKE_COMMAND}" <SOURCE_DIR>/host
         -G ${CMAKE_GENERATOR}
